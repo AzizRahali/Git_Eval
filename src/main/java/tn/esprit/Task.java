3277e73cc365db124a72bc3fcece1f7ed2245360
melek.homrani <melek.homrani@esprit.tn>
2023-11-04 14:10:59 +0100

    update project structure

---
+++ src/main/java/tn/esprit/Task.java
@@ -0,0 +1,79 @@
+package tn.esprit;
+
+import java.util.Objects;
+
+public class Task {
+    int id;
+    String name;
+    String description;
+    boolean done;
+
+    public Task() {
+        this.done = false;
+    }
+
+    public Task(int id, String name, String description, boolean done) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.done = done;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id && done == task.done && Objects.equals(name, task.name) && Objects.equals(description, task.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, done);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", done=" + done +
+                '}';
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+
+    }
+}
