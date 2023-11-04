1da52ab9ba039bc013829049f373a803b0663066
melek.homrani <melek.homrani@esprit.tn>
2023-11-04 11:49:46 +0100

    create todo list class

---
+++ src/ToDoList.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ToDoList {
+    private List<Task> tasks;
+
+    public ToDoList() {
+        tasks = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void removeTask(Task task) {
+        tasks.remove(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return tasks;
+    }
+
+    @Override
+    public String toString() {
+        return "ToDoList{" +
+                "tasks=" + tasks +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ToDoList toDoList = (ToDoList) o;
+        return Objects.equals(tasks, toDoList.tasks);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tasks);
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public Task findTaskById(int id) {
+        for (Task task : tasks) {
+            if (task.getId() == id) {
+                return task;
+            }
+        }
+        return null;
+    }
+}
