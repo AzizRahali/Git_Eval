70b1f0a10394d0200f48eddee7f0733ee9ebca1c
AzizRahali <147147915+AzizRahali@users.noreply.github.com>
2023-11-04 11:09:44 +0100

    Task added

---
+++ src/Task.java
@@ -0,0 +1,37 @@
+public class Task {
+    private int id;
+    private String name;
+    private String status;
+
+    public Task(int id, String name, String status) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+    }
+
+    // Ajoutez des getters et des setters pour les attributs si nécessaire.
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
